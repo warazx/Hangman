5269ef9e5f390c0c5730029cad52669ee5520ada
Christian Karlsson <krille.karlsson@gmail.com>
2016-10-18 12:07:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/christiankarlsson/hangman/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.christiankarlsson.hangman;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void aboutApp(View view) {
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
+    }
+
+}
